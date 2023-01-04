89888bdf9cf7556ebcd4d8d987e2a87d23f452f5
0rc1nus <mitchellctlui@gmail.com>
2023-01-04 19:00:05 +0800

    Fixed boat rendering

---
+++ src/main/java/net/sheddmer/abundant_atmosphere/events/ClientEvents.java
@@ -0,0 +1,35 @@
+package net.sheddmer.abundant_atmosphere.events;
+
+import net.minecraft.client.model.BoatModel;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.sheddmer.abundant_atmosphere.AbundantAtmosphere;
+import net.sheddmer.abundant_atmosphere.client.renderer.AABoatEntityRenderer;
+import net.sheddmer.abundant_atmosphere.entities.AABoatEntity;
+import net.sheddmer.abundant_atmosphere.init.AAEntityTypes;
+import net.sheddmer.abundant_atmosphere.init.AAModelLayers;
+
+import java.util.Arrays;
+
+@OnlyIn(Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = AbundantAtmosphere.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientEvents {
+
+    @SubscribeEvent
+    public static void registerEntityModelLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        Arrays.stream(AABoatEntity.BoatType.values()).forEach(type -> {
+            event.registerLayerDefinition(AAModelLayers.createBoat(type), () -> BoatModel.createBodyModel(false));
+            event.registerLayerDefinition(AAModelLayers.createChestBoat(type), () -> BoatModel.createBodyModel(true));
+        });
+    }
+
+    @SubscribeEvent
+    public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(AAEntityTypes.BOAT.get(), ctx -> new AABoatEntityRenderer(ctx, false));
+        event.registerEntityRenderer(AAEntityTypes.CHEST_BOAT.get(), ctx -> new AABoatEntityRenderer(ctx, true));
+    }
+
+}
